408a01b0af02a8987d3f41f9518629976a036dd5
Naweenl <124073190+Naweenl@users.noreply.github.com>
2023-02-01 14:28:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-/*
+Ò/*
    ██████╗ ██╗   ██╗███████╗███████╗███╗   ██╗      ███╗   ███╗██████╗ 
   ██╔═══██╗██║   ██║██╔════╝██╔════╝████╗  ██║      ████╗ ████║██╔══██╗
   ██║   ██║██║   ██║█████╗  █████╗  ██╔██╗ ██║█████╗██╔████╔██║██║  ██║
@@ -21,9 +21,9 @@ global.APIKeys = {
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-global.session = process.env.SESSION_ID || 'PUT HERE',
-global.ownername = process.env.OWNER_NAME === undefined ? "DarkWinzo" : process.env.OWNER_NAME
-let fake = process.env.OWNER_NUMBER === undefined ? '94775200935' : process.env.OWNER_NUMBER
+global.session = process.env.SESSION_ID || 'TlhLOVdjTlo=',
+global.ownername = process.env.OWNER_NAME === undefined ? "DarKNaviya" : process.env.OWNER_NAME
+let fake = process.env.OWNER_NUMBER === undefined ? '94723396373' : process.env.OWNER_NUMBER
 global.owner = [`${fake}`]
 global.ownernumber = [`${fake}`]
 global.premium = [`${fake}`]
